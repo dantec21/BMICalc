e05eb148e69421bde82811a3dd41b1666d27c81f
Dante <dante.collier.2022@student.lhschools.org>
2019-02-12 14:07:15 -0500

    Introduction

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -11,5 +11,10 @@ public class Main {
 
 
         System.out.println("Hello "+(name)+", my name is Dante.");
+        System.out.println("Today I will be helping you find your body mass index.");
+        System.out.println("I will need your weight in pounds, and your height in feet and inches.");
+        
+
+
     }
 }
