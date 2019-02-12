ddbcf5283b1954e050683156b1494e829f6cb2ad
Dante <dante.collier.2022@student.lhschools.org>
2019-02-12 14:19:17 -0500

    End of day 2-12-19

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -5,15 +5,19 @@ public class Main {
     public static void main(String[] args) {
 	    //initialize
        String name;
+       String heightFeet;
+       String heightInches;
        //Declare
         name = "joseph";
+        heightFeet = "5";
+        heightInches = "11";
 
 
 
         System.out.println("Hello "+(name)+", my name is Dante.");
         System.out.println("Today I will be helping you find your body mass index.");
         System.out.println("I will need your weight in pounds, and your height in feet and inches.");
-        
+
 
 
     }
