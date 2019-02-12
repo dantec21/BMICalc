11f93f7c82d10a2f88640ff79514b0d8dfd603c9
Dante <dante.collier.2022@student.lhschools.org>
2019-02-12 14:05:41 -0500

    Greeting

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,15 @@
+package com.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    //initialize
+       String name;
+       //Declare
+        name = "joseph";
+
+
+
+        System.out.println("Hello "+(name)+", my name is Dante.");
+    }
+}
