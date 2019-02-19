e00fa9640f0e1cd02ac00dd790745f9ea5409b12
Dante <dante.collier.2022@student.lhschools.org>
2019-02-19 14:10:25 -0500

    Complete  Code

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -9,20 +9,32 @@ public class Main {
         Scanner keyboard =new Scanner(System.in);
 	    //initialize
        String name ="Please type your name here";
-       int heightFeet = keyboard.nextLine(), heightInches =8, weightpounds = 1;
+       String heightFeet;
+       int heightInFeet,heightInInches;
+       int weightPounds;
 
-       int  heightinFeet = heightFeet,heightinInches= heightInches,weightInPounds = weightpounds;
-       double feetToMeters = 0.3048,inchesToMeters = 0.0254,poundsToKilograms = 0.453592;
-       System.out.println((heightinFeet*feetToMeters));
-        System.out.println((heightinFeet*feetToMeters)+(heightinInches*inchesToMeters));
+        System.out.println("Hello my name is Dante, today I will be helping you find your body mass index.");
+        System.out.println("What is your height in feet?");
+        heightInFeet = keyboard.nextInt();
+        System.out.println("What is you height in inches?");
+        heightInInches = keyboard.nextInt();
+        System.out.println("What is your weight in pounds?");
+        weightPounds = keyboard.nextInt();
 
-       double heightInMeters = ((heightinFeet*feetToMeters)+(heightinInches*inchesToMeters));
-       double weightInKilograms =weightInPounds*poundsToKilograms;
-       double bmi =weightInKilograms/(heightInMeters* heightInMeters);
 
-       System.out.println("Hello "+(name)+", my name is Dante.");
-       System.out.println("Today I will be helping you find your body mass index.");
-       System.out.println("I will need your weight in pounds, and your height in feet and inches.");
+
+
+        double feetToMeters = 0.3048,inchesToMeters = 0.0254,poundsToKilograms = 0.453592;
+        double heightInMeters = ((heightInFeet*feetToMeters)+(heightInInches*inchesToMeters));
+        double weightInKilograms =weightPounds*poundsToKilograms;
+        double bmi =weightInKilograms/(heightInMeters* heightInMeters);
+
+
+
+
+
+
+
        System.out.println("Your Body Mass Index is "+(Math.round(bmi))+".");
         if (bmi<18.5)
             System.out.println("Your body mass index is considered underweight.");
